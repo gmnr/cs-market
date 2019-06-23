24e2da6296d26b647574024148e92c16bcf37202
gmnr <guido.minieri@gmail.com>
2019-06-23 21:37:24 +0200

    first implementation running, without logging / error handling

---
+++ main.py
@@ -0,0 +1,46 @@
+# connects to the loclahost db with the reference index
+from config import *   # credentials and table names
+import psycopg2
+from datetime import datetime
+import requests
+import json
+
+
+# define the url to access the steam market api
+url = 'https://steamcommunity.com/market/priceoverview/?%20country=IT&currency=3&appid=730&market_hash_name={}%20%7C%20{}%20%28{}%29'
+
+# connect to the database
+conn = psycopg2.connect(user=USER, dbname=DB)
+
+
+# create the cursor
+c = conn.cursor()
+c.execute("SELECT id, name, color, wear FROM {}".format(ITEM_DB))
+records = c.fetchall()
+
+
+# loop and write to db
+for item in records:
+
+    # unpack the 3 variables
+    item_id, name, color, wear = item
+
+    r = requests.get(url.format(name, color, wear))
+    result = r.json()
+
+    low = result['lowest_price'][:-1].replace(',', '.')   # strip the euro sybmol and convert in the . format
+    med = result['median_price'][:-1].replace(',', '.')   # strip the euro symbol and convert in the . format
+    vol = result['volume'].replace(',', '')   # strip . 
+    dt = datetime.now()
+
+    # write into the db
+    c.execute("INSERT INTO market(executed_on, item_id, volume, lowest_price, median_price) values('{}', '{}', '{}', '{}', '{}');".format(dt, item_id, vol, low, med))
+    print('Executed for', name)
+
+# make changes permanent
+conn.commit()
+
+# clear the cursor and close the connection
+if (conn):
+    c.close()
+    conn.close()
